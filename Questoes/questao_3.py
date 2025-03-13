c6389e49324fc953904281b4c3ebafae77193b7e
Arthur Fornaciari Gaviorno <arthurfornaciarig@gmail.com>
2025-03-12 22:34:09 -0300

    Update questao_3.py
    adicionados comentarios e efetuadas algumas alteracoes no codigo

--- Questoes/questao_3.py
+++ Questoes/questao_3.py
@@ -1,15 +1,15 @@
 import json
 
-with open("dados.json") as faturamento:
+with open("dados.json") as faturamento: 
     dados = json.load(faturamento)
     
-valores = [i['valor'] for i in dados if i['valor'] > 0]
-menor = min(valores)
-maior = max(valores)
+valores = [i['valor'] for i in dados if i['valor'] > 0] #cria uma variavel que armazena todos os valores da base de dados que sao maiores que 0
+menor = min(valores) #funcao que calcula o minimo
+maior = max(valores) #funcao que calcula o maximo
 
-media = sum(valores) / len(valores)
-maior_media = sum(1 for i in dados if i > media)
+media = sum(valores) / len(valores) #funcao para calculo da media, e abaixo a funcao que soma 1 toda vez que o faturamento for maior que a media
+maior_media = sum(1 for i in valores if i > media) 
 
 print('Menor Faturamento: R$', menor)
 print('Maior Faturamento: R$', maior)
-print('Quantidade de dias em que faturamento foi maior que media', maior_media)
+print('Quantidade de dias em que faturamento foi maior que media: ', maior_media)
