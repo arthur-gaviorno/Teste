59787fb0eb48527d1c5106c84c07fa066a2198e5
Arthur Fornaciari Gaviorno <arthurfornaciarig@gmail.com>
2025-03-12 22:16:41 -0300

    Create questao_1.py

---
+++ Questoes/questao_1.py
@@ -0,0 +1,9 @@
+indice = 13
+soma = 0
+k = 0
+
+while (k < indice - 1): #indice - 1 para nao atingir k == 13                                                             
+    k = k + 1
+    soma = soma + k
+   
+print(soma)
